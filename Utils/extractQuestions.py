2b7c237ae82a5665d1de391831abb4bc51096bfe
Lasse Drongesen <Lassemgp@gmail.com>
2025-02-21 13:40:07 +0100

    data file for translation generated, util file for reading data json and creating new json added (can be used for inspiration)

--- Utils/extractQuestions.py
+++ Utils/extractQuestions.py
@@ -1,1 +1,16 @@
+import json
+data_path = '../data/mintaka_dev.json'
+output_path = '../data/id2question.json'
 # util for extracting all the questions in english from the mintaka dataset
+def extract_questions(data_path, output_file):
+    with open(data_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    questions = {item['id']: item['question'] for item in data}
+
+    # Write the new dictionary to the output file
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(questions, f, indent=4)
+
+# extract_questions(data_path, output_path)
+
