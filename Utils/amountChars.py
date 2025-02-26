fba0503e2925870d4f327076852721f5356f85f3
Lasse Drongesen <Lassemgp@gmail.com>
2025-02-26 11:23:48 +0100

    processing of questions pipeline

--- Utils/amountChars.py
+++ Utils/amountChars.py
@@ -1,12 +1,12 @@
 
 import json
 
-with open("../data/id2question_dev.json", 'r', encoding='utf-8') as f:
-        data = json.load(f)
 
-char_count = 0
-for key, value in data.items():
-    char_count += len(value)
+def amountChars(questions):
+    char_count = 0
+    for question in questions:
+        char_count += len(question)
+    return char_count
 
-print(f"Der er {char_count} bogstaver i alt i alle spørgsmålene der skal oversættes")
+
 # 112754
--- Utils/extractQuestions.py
+++ Utils/extractQuestions.py
@@ -1,16 +1,18 @@
 import json
-data_path = '../data/mintaka_train.json'
-output_path = '../data/id2question_train.json'
+from Utils.amountChars import amountChars
+
 # util for extracting all the questions in english from the mintaka dataset
-def extract_questions(data_path, output_file):
+def extract_questions(data_path, output_path):
     with open(data_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     questions = {item['id']: item['question'] for item in data}
-
+    chars = amountChars(questions.values())
+    print(f"Extracted {len(questions)} questions with a total of {chars} characters")
+    questions = {"questions" : questions, "char_count": chars}
     # Write the new dictionary to the output file
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_path, 'w', encoding='utf-8') as f:
         json.dump(questions, f, indent=4)
 
-extract_questions(data_path, output_path)
+
 
