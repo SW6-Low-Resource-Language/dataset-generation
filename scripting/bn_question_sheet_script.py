878bf3e98f680dcd5a896c3173c5c347a43d6972
Lasse Drongesen <Lassemgp@gmail.com>
2025-05-12 14:03:34 +0200

    finnish spotcheck data

--- scripting/bn_question_sheet_script.py
+++ scripting/bn_question_sheet_script.py
@@ -2,17 +2,17 @@ from openpyxl import Workbook, load_workbook
 from shared_utils.file_service import open_txt
 
 
-da_wb = load_workbook("./outputs/sampling/sampled_translations.xlsx")
+da_wb = load_workbook("../outputs/sampling/sampled_translations_checked.xlsx")
 da_ws = da_wb.active
 
 
 
-english_qsts = open_txt("./outputs/questions_txt_files/dev_questions.txt")
+english_qsts = open_txt("../outputs/questions_txt_files/dev_questions.txt")
 qst_index_map = {}
 for index, qst in enumerate(english_qsts):
     qst_index_map[qst] = index
 
-bengali_qsts = open_txt("./outputs/translations/google/dev_questions_bn_linebyline.txt")
+finish_qsts = open_txt("../outputs/translations/fi/dev_questions_fi.txt")
 
 questions_for_sampling = []
 
@@ -21,17 +21,17 @@ for row in da_ws.iter_rows(min_row=2, max_row=da_ws.max_row, min_col=1, max_col=
     print(row)
     eng_qst = row[0]
     index = qst_index_map[eng_qst]
-    bn_qst = bengali_qsts[index]
+    bn_qst = finish_qsts[index]
     questions_for_sampling.append([eng_qst,bn_qst])
 
 
 bn_wb = Workbook()
 bn_ws = bn_wb.active
-bn_ws.append(["English", "Bengali"])
+bn_ws.append(["English", "Finnish"])
 for qsts in questions_for_sampling:
     bn_ws.append(qsts)
 
-bn_wb.save("./outputs/sampling/sampled_translations_bn.xlsx")
+bn_wb.save("../outputs/sampling/sampled_translations_fi.xlsx")
 
 
 
